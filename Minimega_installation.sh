bac88412237cc9cf85f37242f16b2c3bf7d5375f
Living Lab @ IUPUI <livlab@iupui.edu>
2015-10-20 18:53:14 -0400

    minimega script now installs LivLab version by default

--- Minimega_installation.sh
+++ Minimega_installation.sh
@@ -1,7 +1,15 @@
 #Installing minimega 2.0 on Ubuntu server 14.*
-wget https://github.com/sandia-minimega/minimega/archive/2.0.zip
-sudo apt-get install unzip
-unzip 2.0.zip
+
+#Original minimega 2.0
+#wget https://github.com/sandia-minimega/minimega/archive/2.0.zip
+#sudo apt-get install unzip
+#unzip 2.0.zip
+
+#LivLab modifications
+sudo apt-get install git
+git clone https://github.com/ITLivLab/minimega
+
+#Normal installation
 sudo apt-get install golang gcc libpcap-dev libreadline-dev dnsmasq qemu-kvm openvswitch-switch #Install dep.
-cd minimega-2.0/
-sudo ./build.bash #Compile
+cd minimega*/
+sudo bash build.bash #Compile
